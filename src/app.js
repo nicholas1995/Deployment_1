a14e55512fc2af874753c86e91c1a9e3f06bbaac
Nicholas Mitchell <nicholasmitchell@nicholass-MacBook-Pro.local>
2019-02-02 00:57:25 -0400

    Commit

--- src/app.js
+++ src/app.js
@@ -4,22 +4,33 @@ const cors = require('cors'); //find out what it does
 const morgan = require('morgan'); //logs which device etc hit the request
 const config = require('./configeration/config');
 const app = express();
-const DB = require('./services/database/device_rx_db')
 
 require("./services/MQTT");
+
+const db = require('./db');
+module.exports = app;
+require('./routes')(app);
+
 
 //app.use(morgan('combined'));
 app.use(bodyParser.json({limit: '50mb'}));//this fixed the bug with the email size
 app.use(cors()); //DO RESEACH ON THIS.... 29:42 on first video
 require('./passport');
+
+//------------NEW FOR DEPLOYMENT-------------
+const path = require("path")
+app.use(express.static(path.join(__dirname, "client", "build")))
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname,"", "../client", "dist", "index.html"));
+});
+//--------------------------------------------
+
+
 //Start Express Server
 app.listen(config.port, () =>{
     console.log('Server started on port: '+ config.port)
 });  
-
-const db = require('./db');
-module.exports = app;
-require('./routes')(app);
 
 
 
