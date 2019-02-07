6b75c58cfef5128adc809d75cef27feb8bdfb249
Nicholas Mitchell <nicholasmitchell@nicholass-MacBook-Pro.local>
2019-02-07 15:31:59 -0400

    m

--- src/configeration/config.js
+++ src/configeration/config.js
@@ -2,7 +2,7 @@
 module.exports = {
     port: process.env.PORT || 3000,
     db:{    
-        host     : 'http://dab3e029.ngrok.io',
+        host     : '127.0.0.1:4040',
         user     : 'root',
         password : '',
         database: 'lora_console'
